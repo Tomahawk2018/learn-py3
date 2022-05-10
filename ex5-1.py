fae613259e959a9ccec1b00a52e78936a978bac8
545227863@qq.com <z1994121576z>
2022-05-10 23:23:20 +0800

    练习5

---
+++ ex5-1.py
@@ -0,0 +1,15 @@
+name = 'Zed A.shaw'
+age = 35 # not a lie
+height = 74 # inches
+weight =  180 #lbs
+eyes = 'blue'
+teeth = 'White'
+hair = 'Brown'
+
+print(f"Let's talk about {name}.")
+print(f"He's {height} inches tall.")
+print(f"He's {weight} pounds heavy")
+print("Actually that's not too heavy")
+print(f"He's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee.")
+print(round(1.7333))
